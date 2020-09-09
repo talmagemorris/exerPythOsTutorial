d75b176433d97a8b5c81ac89cf7d56c3cd87a374
talmagemorris <talmagemorris1@gmail.com>
2020-09-08 20:02:59 -0600

    uploading PythOsTutorial

---
+++ os-tut.py
@@ -0,0 +1,19 @@
+import os
+
+#print(os.listdir)
+
+os.chdir('/OneDrive/Documents/School/Computer science/OSmoduleTutorial/exerPythOsTutorial')
+
+#for dirpath, dirnames, filenames in os.walk('/OneDrive/Documents/School/Computer science/OSmoduleTutorial/exerPythOsTutorial')
+    print('Current Path:' , dirpath)
+    print('Directories:', dirnames)
+    print('Files:', filenames)
+
+#file_path = os.path.join(os.environ.get('talma')), 'test.txt')
+print(file_path)
+
+print(os.path.basename('/tmp/test.txt'))
+
+#I am supposed to enter a comment with a URL for official python documentation 
+
+#https://www.youtube.com/watch?v=tJxcKyFMTGo
